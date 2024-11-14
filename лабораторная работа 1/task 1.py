c4548b38ad5e915d990ba859e2d3c5b54e5d5dbc
Anastasiya-proger <kudryavtseva.a.v.tutor@gmail.com>
2024-11-14 16:19:21 +0300

    Update task 1.py

--- лабораторная работа 1/task 1.py
+++ лабораторная работа 1/task 1.py
@@ -1,17 +1,8 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-total_sum=0
-count=0
-for num in numbers:
-    if num is not None:
-        total_sum += num
-        count += 1
-average= total_sum / (count + 1)
-
-for i in range(len(numbers)):
-    if numbers[i] is None:
-        numbers[i] = average
-        break
-
+missing_index = numbers.index(None)
+new_numbers = numbers[:missing_index] + numbers[missing_index + 1:]
+average = sum(new_numbers) / (len (new_numbers) +1 )
+numbers[missing_index] = average
 
 
 # TODO заменить значение пропущенного элемента средним арифметическим
